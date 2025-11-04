2613775c7dc41d3be266eadb711b4bcf7f20d603
Jason Dempsey <jasondemps1@gmail.com>
2025-11-03 23:06:25 -0500

    added tilemap and level

---
+++ src/assets/dungeon.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="dungeon" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+ <image source="level_tilemap.png" width="192" height="176"/>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="4" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
